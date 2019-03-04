2cbb24a288ce9013706f722285c087a613584a40
Vyacheslav <theelecman@gmail.com>
2019-03-05 01:48:04 +0300

    Add files via upload

---
+++ website/dist/js/scripts.js
@@ -0,0 +1,171 @@
+'use strict';
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+/******/(function (modules) {
+  // webpackBootstrap
+  /******/ // The module cache
+  /******/var installedModules = {};
+  /******/
+  /******/ // The require function
+  /******/function __webpack_require__(moduleId) {
+    /******/
+    /******/ // Check if module is in cache
+    /******/if (installedModules[moduleId]) {
+      /******/return installedModules[moduleId].exports;
+      /******/
+    }
+    /******/ // Create a new module (and put it into the cache)
+    /******/var module = installedModules[moduleId] = {
+      /******/i: moduleId,
+      /******/l: false,
+      /******/exports: {}
+      /******/ };
+    /******/
+    /******/ // Execute the module function
+    /******/modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+    /******/
+    /******/ // Flag the module as loaded
+    /******/module.l = true;
+    /******/
+    /******/ // Return the exports of the module
+    /******/return module.exports;
+    /******/
+  }
+  /******/
+  /******/
+  /******/ // expose the modules object (__webpack_modules__)
+  /******/__webpack_require__.m = modules;
+  /******/
+  /******/ // expose the module cache
+  /******/__webpack_require__.c = installedModules;
+  /******/
+  /******/ // define getter function for harmony exports
+  /******/__webpack_require__.d = function (exports, name, getter) {
+    /******/if (!__webpack_require__.o(exports, name)) {
+      /******/Object.defineProperty(exports, name, { enumerable: true, get: getter });
+      /******/
+    }
+    /******/
+  };
+  /******/
+  /******/ // define __esModule on exports
+  /******/__webpack_require__.r = function (exports) {
+    /******/if (typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+      /******/Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+      /******/
+    }
+    /******/Object.defineProperty(exports, '__esModule', { value: true });
+    /******/
+  };
+  /******/
+  /******/ // create a fake namespace object
+  /******/ // mode & 1: value is a module id, require it
+  /******/ // mode & 2: merge all properties of value into the ns
+  /******/ // mode & 4: return value when already ns object
+  /******/ // mode & 8|1: behave like require
+  /******/__webpack_require__.t = function (value, mode) {
+    /******/if (mode & 1) value = __webpack_require__(value);
+    /******/if (mode & 8) return value;
+    /******/if (mode & 4 && (typeof value === 'undefined' ? 'undefined' : _typeof(value)) === 'object' && value && value.__esModule) return value;
+    /******/var ns = Object.create(null);
+    /******/__webpack_require__.r(ns);
+    /******/Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+    /******/if (mode & 2 && typeof value != 'string') for (var key in value) {
+      __webpack_require__.d(ns, key, function (key) {
+        return value[key];
+      }.bind(null, key));
+    } /******/return ns;
+    /******/
+  };
+  /******/
+  /******/ // getDefaultExport function for compatibility with non-harmony modules
+  /******/__webpack_require__.n = function (module) {
+    /******/var getter = module && module.__esModule ?
+    /******/function getDefault() {
+      return module['default'];
+    } :
+    /******/function getModuleExports() {
+      return module;
+    };
+    /******/__webpack_require__.d(getter, 'a', getter);
+    /******/return getter;
+    /******/
+  };
+  /******/
+  /******/ // Object.prototype.hasOwnProperty.call
+  /******/__webpack_require__.o = function (object, property) {
+    return Object.prototype.hasOwnProperty.call(object, property);
+  };
+  /******/
+  /******/ // __webpack_public_path__
+  /******/__webpack_require__.p = "";
+  /******/
+  /******/
+  /******/ // Load entry module and return exports
+  /******/return __webpack_require__(__webpack_require__.s = "./website/src/js/scripts.js");
+  /******/
+})(
+/************************************************************************/
+/******/{
+
+  /***/"./website/src/js/primary.js":
+  /*!***********************************!*\
+    !*** ./website/src/js/primary.js ***!
+    \***********************************/
+  /*! exports provided: default */
+  /***/function websiteSrcJsPrimaryJs(module, __webpack_exports__, __webpack_require__) {
+
+    "use strict";
+
+    __webpack_require__.r(__webpack_exports__);
+    /* harmony default export */__webpack_exports__["default"] = function () {
+      return 'Primary Script';
+    };
+
+    /***/
+  },
+
+  /***/"./website/src/js/scripts.js":
+  /*!***********************************!*\
+    !*** ./website/src/js/scripts.js ***!
+    \***********************************/
+  /*! no exports provided */
+  /***/function websiteSrcJsScriptsJs(module, __webpack_exports__, __webpack_require__) {
+
+    "use strict";
+
+    __webpack_require__.r(__webpack_exports__);
+    /* harmony import */var _primary__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__( /*! ./primary */"./website/src/js/primary.js");
+
+    var Person = function () {
+      function Person(name) {
+        _classCallCheck(this, Person);
+
+        this.name = name;
+      }
+
+      _createClass(Person, [{
+        key: 'hello',
+        value: function hello() {
+          if (typeof this.name === 'string') return 'Hello, I am ' + this.name + '!';
+          return 'Hello!';
+        }
+      }]);
+
+      return Person;
+    }();
+
+    var harry = new Person('Harman Manchanda');
+    console.log(harry.hello());
+    console.log(Object(_primary__WEBPACK_IMPORTED_MODULE_0__["default"])());
+
+    /***/
+  }
+
+  /******/ });
+//# sourceMappingURL=scripts.js.map
